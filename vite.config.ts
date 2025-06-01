880a76e4d526742cba2b3e40603ae503319449d6
itamarnav <itamar.bl@navina.ai>
2025-06-01 23:31:42 +0300

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
-  test: {
-    environment: 'jsdom',
-  },
+  base: '/morgen/',
+  css: { postcss: './postcss.config.js' },
 });
